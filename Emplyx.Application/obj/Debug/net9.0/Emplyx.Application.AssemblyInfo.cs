646b9d83ea87976fe7a472802458d8f2f3b222b4
emplyxrrhh <emplyxrrhh@gmail.com>
2025-11-20 19:57:13 +0100

    pasar a vs 26

--- Emplyx.Application/obj/Debug/net9.0/Emplyx.Application.AssemblyInfo.cs
+++ Emplyx.Application/obj/Debug/net9.0/Emplyx.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Emplyx.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1907f51b5382e5cc881d8f2a910b1dfdff4c503c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fec2c331442ca83bbc26184350799277574fe56d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Emplyx.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Emplyx.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
